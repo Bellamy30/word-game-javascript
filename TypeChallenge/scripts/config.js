d17d1e8959a2a823ce29d7d463d776a065d2913e
Bellamy30 <billajordan78@gmail.com>
2026-05-17 00:31:56 +0100

    Delete TypeChallenge directory

--- TypeChallenge/scripts/config.js
+++
@@ -1,5 +0,0 @@
-const listePhrases = ["Pas de panique !", "La vie, l’univers et le reste", "Merci pour le poisson"];
-const listeMots = ["Cachalot", "Petunia", "Serviette"];
-const erreurs = [];
-let motOuPhraseEntree = [];
-let motOuPhraseUtilisateur;
